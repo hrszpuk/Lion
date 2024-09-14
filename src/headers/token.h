e76544cc3d5a1bc9e832465f95ce915d46bdeddd
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-14 23:09:05 +0100

    Added Token class declaration

---
+++ src/headers/token.h
@@ -0,0 +1,31 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <string>
+
+enum class TokenType {
+    INTEGER,        // [0-9]+
+    PLUS,           // +
+    MINUS,          // -
+    MUL,            // *
+    DIV,            // /
+    LPAREN,         // (
+    RPAREN          // )
+};
+
+class Token {
+public:
+  	TokenType type;
+    std::string value;
+
+    Token();
+    Token(int type, std::string value);
+
+    Token(TokenType type, std::string value);
+
+    TokenType getType();
+    std::string getValue();
+    std::string toString();
+};
+
+#endif //TOKEN_H
