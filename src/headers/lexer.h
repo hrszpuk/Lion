2bf4b7d191a677ed2a1bcd9eeea57bdb632b1f45
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-15 23:09:45 +0100

    Added Lexer declaration

--- src/headers/lexer.h
+++ src/headers/lexer.h
@@ -1,8 +1,24 @@
-//
-// Created by hrs on 16/09/24.
-//
-
 #ifndef LEXER_H
 #define LEXER_H
+
+#include <vector>
+
+#include "token.h"
+
+class Lexer {
+public:
+    std::vector<Token> tokens;
+    std::string text;
+    int pos;
+    char currentChar;
+
+    Lexer(std::string text);
+
+    std::vector<Token> lex();
+    Token lexInteger();
+    void advance();
+    void skipWhitespace();
+    void error();
+};
 
 #endif //LEXER_H
