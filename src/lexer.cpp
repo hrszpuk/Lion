8873005956193d2c9b735b1a2a58bbc98139255c
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-15 23:09:56 +0100

    Added Lexer implementation skeleton

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,5 +1,30 @@
-//
-// Created by hrs on 16/09/24.
-//
-
 #include "headers/lexer.h"
+
+#include <iostream>
+#include <vector>
+
+Lexer::Lexer(std::string text) {
+    this->text = text;
+    this->pos = 0;
+    this->currentChar = this->text[this->pos];
+}
+
+std::vector<Token> Lexer::lex() {
+
+}
+
+Token Lexer::lexInteger() {
+
+}
+
+void Lexer::advance() {
+
+}
+
+void Lexer::skipWhitespace() {
+
+}
+
+void Lexer::error() {
+
+}
