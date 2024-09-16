09d3cf07951db1ce70437183551e7299c758737e
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-16 23:28:13 +0100

    Added lexer implementation

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -10,21 +10,73 @@ Lexer::Lexer(std::string text) {
 }
 
 std::vector<Token> Lexer::lex() {
-
+    while (this->currentChar != '\0') {
+        if (this->currentChar == ' ') {
+            this->skipWhitespace();
+            continue;
+        }
+        if (isdigit(this->currentChar)) {
+            this->tokens.push_back(this->lexInteger());
+            continue;
+        }
+        switch (this->currentChar) {
+            case '+':
+                this->tokens.push_back(Token(TokenType::PLUS, "+"));
+                this->advance();
+                break;
+            case '-':
+                this->tokens.push_back(Token(TokenType::MINUS, "-"));
+                this->advance();
+                break;
+            case '*':
+                this->tokens.push_back(Token(TokenType::MUL, "*"));
+                this->advance();
+                break;
+            case '/':
+                this->tokens.push_back(Token(TokenType::DIV, "/"));
+                this->advance();
+                break;
+            case '(':
+                this->tokens.push_back(Token(TokenType::LPAREN, "("));
+                this->advance();
+                break;
+            case ')':
+                this->tokens.push_back(Token(TokenType::RPAREN, ")"));
+                this->advance();
+                break;
+            default:
+                this->error();
+                break;
+        }
+    }
+    return this->tokens;
 }
 
 Token Lexer::lexInteger() {
-
+    std::string result = "";
+    while (this->currentChar != '\0' && isdigit(this->currentChar)) {
+        result += this->currentChar;
+        this->advance();
+    }
+    return Token(TokenType::INTEGER, result);
 }
 
 void Lexer::advance() {
-
+    this->pos++;
+    if (this->pos > this->text.length() - 1) {
+        this->currentChar = '\0';
+    } else {
+        this->currentChar = this->text[this->pos];
+    }
 }
 
 void Lexer::skipWhitespace() {
-
+    if (this->currentChar == ' ') {
+        this->advance();
+    }
 }
 
 void Lexer::error() {
-
+    std::cout << "Invalid character" << std::endl;
+    this->pos++;
 }
