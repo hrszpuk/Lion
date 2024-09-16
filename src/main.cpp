de4a2b1b9a10dbf33c48ccb1c713437f237efde8
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-16 23:28:26 +0100

    Added a basic test to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "headers/lexer.h"
 
 int main(int argc, char *argv[]) {
+    std::string text = "1 + 2 * 3";
+    Lexer lexer(text);
+    std::vector<Token> tokens = lexer.lex();
+    for (Token token : tokens) {
+        std::cout << token.toString() << std::endl;
+    }
     return 0;
 }
