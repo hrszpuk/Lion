99b3e45914ad5a3dde3e2aa968bdfdae96ed90c8
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-25 15:57:12 +0100

    Fixed GTest for test_token (columnStart/End caused failure)

--- tests/test_token.cpp
+++ tests/test_token.cpp
@@ -12,7 +12,7 @@ TEST(TokenTest, ValidConstructorTest) {
 
 // Test to verify the getters return the expected values
 TEST(TokenTest, GettersTest) {
-    const Position p {1, 1, 2, 3};
+    const Position p {1, 1, 2, 2, 3};
     const Token t(TokenType::LET_KW, std::string("let"), p);
 
     EXPECT_EQ(t.getType(), TokenType::LET_KW);
@@ -22,7 +22,7 @@ TEST(TokenTest, GettersTest) {
 
 // Test to verify complete constructor initialises correctly
 TEST(TokenTest, SpanInitializationTest) {
-    const Position pos(1, 1, 10, 13);
+    const Position pos(1, 1, 2, 10, 13);
     const Token t(TokenType::LET_KW, std::string("let"), pos);
 
     EXPECT_EQ(t.getPosition(), pos);
@@ -32,7 +32,7 @@ TEST(TokenTest, SpanInitializationTest) {
 TEST(TokenTest, ToStringTest) {
     const Token t(TokenType::LET_KW, "let");
 
-    EXPECT_EQ(t.toString(), "Token(1, let)");
+    EXPECT_EQ(t.toString(), "Token(LET_KW, \"let\", (0:0-0)[0..0])");
 }
 
 // Test for empty string value handling
@@ -52,30 +52,31 @@ TEST(TokenTest, InvalidTokenTypeTest) {
 // Test to verify line and column are not 0
 TEST(PositionTest, InvalidLineColumnTest) {
     EXPECT_THROW({
-        Token t(TokenType::LET_KW, "let", Position{0, 0, 0, 1});
+        Token t(TokenType::LET_KW, "let", Position{0, 0, 1, 0, 1});
     }, std::invalid_argument);
 }
 
 // Test to verify end is greater than start
 TEST(PositionTest, InvalidStartEndTest) {
     EXPECT_THROW({
-        Token t(TokenType::LET_KW, "let", Position{1, 1, 10, 0});
+        Token t(TokenType::LET_KW, "let", Position{1, 1, 2, 10, 0});
     }, std::invalid_argument);
 }
 
 // Test to verify end is greater than start
 TEST(PositionTest, InvalidEqualStartEndTest) {
     EXPECT_THROW({
-        Token t(TokenType::LET_KW, "let", Position{1, 1, 0, 0});
+        Token t(TokenType::LET_KW, "let", Position{1, 1, 2, 0, 0});
     }, std::invalid_argument);
 }
 
 // Test to verify position getters work
 TEST(PositionTest, GetterTest) {
-    const Position p(1, 1, 10, 13);
+    const Position p(1, 1, 10, 10, 13);
 
     EXPECT_EQ(p.getLine(), 1);
-    EXPECT_EQ(p.getColumn(), 1);
+    EXPECT_EQ(p.getColumnStart(), 1);
+    EXPECT_EQ(p.getColumnEnd(), 10);
     EXPECT_EQ(p.getStart(), 10);
     EXPECT_EQ(p.getEnd(), 13);
 }
